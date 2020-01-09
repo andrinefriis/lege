c97b61525e77f1ef29f11f59db0181fe233933d4
Andrine Friis Hausken <andrinefriis@hotmail.com>
2020-01-09 09:03:28 +0100

    hie

---
+++ db-components1/legekontor.sql
@@ -0,0 +1,60 @@
+-- Demo database for testing of db-components
+-- originally intended for Norwegian students
+-- english names would be customer,product,order,detail
+
+-- create role,database for demo
+create role legekontor password '123';     -- legekontor is user
+alter role legekontor with login;          -- allow login
+create database legekontor owner legekontor;     -- create db
+
+-- enter the new db
+\c legekontor;
+
+
+
+DROP TABLE IF EXISTS pasient cascade;
+DROP TABLE IF EXISTS lege cascade;
+DROP TABLE IF EXISTS legetime cascade;
+DROP TABLE IF EXISTS linje cascade;
+
+
+
+-- pasient
+CREATE TABLE pasient (
+  pasientid SERIAL PRIMARY KEY,
+  fornavn text NOT NULL,
+  etternavn text NOT NULL,
+  adresse text,
+  epost text,
+  tlf text,
+  kjonn text,
+  userid int unique not null
+);
+
+-- lege
+CREATE TABLE  lege  (
+   legeid  SERIAL PRIMARY KEY,
+   fornavn  text NOT NULL,
+   etternavn text NOT NULL,
+   tlf text
+);
+
+-- legetime
+CREATE TABLE  legetime  (
+   legetimeid  SERIAL PRIMARY KEY,
+   dato  date NOT NULL,
+   kl time,
+   legeid  int REFERENCES lege (legeid)
+);
+
+
+ALTER TABLE  legetime  ADD FOREIGN KEY ( pasientid ) REFERENCES  pasient  ( pasientid );
+ALTER TABLE  linje  ADD FOREIGN KEY ( legetimeid ) REFERENCES  legetime  ( legetimeid );
+ALTER TABLE  linje  ADD FOREIGN KEY ( legeid ) REFERENCES  lege  ( legeid );
+ALTER TABLE  pasient  ADD FOREIGN KEY ( userid ) REFERENCES  users  ( userid );
+ALTER TABLE  lege  ADD FOREIGN KEY ( kategori ) REFERENCES  lege  ( legeid );
+
+alter table legetime owner to legekontor;
+alter table lege owner to legekontor;
+alter table pasient owner to legekontor;
+
